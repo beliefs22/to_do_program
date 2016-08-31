7486273ab88adbc7c214c84531cc02c8e758646a
beliefs22 <beliefs22@gmail.com>
2016-08-30 21:27:17 -0400

    Updating command.py

--- commands.py
+++ commands.py
@@ -1,8 +1,27 @@
-def add_project(name):
-    pass
+import sqlite3
 
-def add_task(name, start, due=None, notes=None):
-    pass
+conn = sqlite3.connect('projectdata.db')
+
+c = conn.cursor()
+
+def add_project(project_title):
+    c.execute('''
+    INSERT INTO projects
+    (project_title)
+    VALUES (?)''',
+    (project_title))
+
+    conn.commit()
+
+def add_task(task_title,project_title,start_date,
+             due_date=None, notes=None,status=None):
+    c.execute('''
+    INSERT INTO tasks
+    (task_title, project_title, start_date, due_date, notes, status)
+    VALUES (?,?,?,?,?,?)''',
+    (task_title, project_title, start_date, due_date, notes, status))
+
+    conn.commit()
 
 def update_task(name,status=None,due=None,notes=None):
     pass
