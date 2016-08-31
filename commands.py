ceb5ac1c733d9dcbc1d6a8fbdcd8f64fa3ec8f47
beliefs22 <beliefs22@gmail.com>
2016-08-30 21:09:22 -0400

    first commit
    Adding database and basic command file

---
+++ commands.py
@@ -0,0 +1,11 @@
+def add_project(name):
+    pass
+
+def add_task(name, start, due=None, notes=None):
+    pass
+
+def update_task(name,status=None,due=None,notes=None):
+    pass
+
+def view_projects(timeframe):
+    pass
